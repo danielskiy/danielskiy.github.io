f35311f08e8f6309a140e3ba9879775b283833b2
danielskiy <129310108+danielskiy@users.noreply.github.com>
2023-11-24 21:53:10 +0100

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,44 +1,36 @@
 <?php
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require './PHPMailer/src/PHPMailer.php';
-require './PHPMailer/src/Exception.php';
-require './PHPMailer/src/SMTP.php';
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
 
-// Initialisiere die Formulardaten
-$sender_name = isset($_POST['name']) ? $_POST['name'] : '';
-$sender_email = isset($_POST['email']) ? $_POST['email'] : '';
-$sender_phone = isset($_POST['Telefon']) ? $_POST['Telefon'] : ''; // Beachte, dass das Telefonfeld im Formular "Telefon" heißt
-$sender_message = isset($_POST['message']) ? $_POST['message'] : '';
+$name = $_POST['name'];
+$tel = $_POST['tel'];
+$email = $_POST['email'];
+$message = $_POST['message'];
 
-// PHPMailer-Setup
 $mail = new PHPMailer(true);
 
-try {
-    //Servereinstellungen
-    $mail->isSMTP();
-    $mail->Host       = 'smtp.gmail.com';
-    $mail->SMTPAuth   = true;
-    $mail->Username   = 'dslutskovsky@gmail.com'; // Gib hier deine Gmail-Adresse ein
-    $mail->Password   = 'yulialex'; // Gib hier dein Gmail-Passwort ein
-    $mail->SMTPSecure = 'tls';
-    $mail->Port       = 587;
+$mail->isSMTP();
+$mail->Host = 'smtp.gmail.com';
+$mail->SMTPAuth = true;
+$mail->Username = 'info@pavilius.de';
+$mail->Password = '';
+$mail->SMTPSecure = 'tls';
+$mail->Port = 587;
 
-    // Empfänger und Betreff
-    $mail->setFrom($sender_email, $sender_name);
-    $mail->addAddress('ziel_email@gmail.com'); // Gib hier die E-Mail-Adresse des Empfängers ein
-    $mail->Subject = 'Betreff deiner E-Mail';
 
-    // Nachricht
-    $mail->Body = "From: $sender_name \n Email: $sender_email \n Phone: $sender_phone \n Message: $sender_message";
+$mail->setFrom('info@pavilius.de', 'Your Name');
+$mail->addAddress('info@pavilius.de', 'Recipient Name');
+$mail->isHTML(true);
+$mail->Subject = 'Test Email';
+$mail->Body = '<h1>New message from your website</h1><p><b>Name: </b>' . $name . '</p><p><b>Tel: </b>' . $tel . '</p><p><b>Email: </b>' . $email . '</p><p><b>Message: </b>' . $message . '</p>';
 
-    // E-Mail senden
-    $mail->send();
-
-    header('Location: thanks.html');
-    exit;
-} catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+if($mail->send()) {
+   echo 'Email sent successfully.';
+} else {
+   echo 'Error sending email: ' . $mail->ErrorInfo;
 }
-?>
